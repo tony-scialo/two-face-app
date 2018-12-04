9df22758bfd202c21b985b3a85ded1bd311e774c
Scialo, Tony <tony_scialo@csx.com>
2018-12-04 10:34:24 -0500

    random image being selected correctly

--- src/app/show-image/show-image.component.ts
+++ src/app/show-image/show-image.component.ts
@@ -1,3 +1,4 @@
+import { FILENAMES } from '../shared/data/image-names';
 import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 import { RootStoreState, HomeSelectors } from '../root-store';
@@ -8,11 +9,23 @@ import { RootStoreState, HomeSelectors } from '../root-store';
   styleUrls: ['./show-image.component.scss']
 })
 export class ShowImageComponent implements OnInit {
+  filenameList = FILENAMES;
+  randomImg: string;
+
   constructor(private store$: Store<RootStoreState.State>) {}
 
   ngOnInit() {
-    this.store$
-      .select(HomeSelectors.selectUserName)
-      .subscribe(name => console.log(name));
+    this.randomImg = this.getRandomImage();
+  }
+
+  generateRandomNumInRange(min, max): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getRandomImage(): string {
+    const t = this.generateRandomNumInRange(0, this.filenameList.length - 1);
+    return this.filenameList[t].filename;
   }
 }
